55d05fd3455e06b941e7ae3c2f40adf0095f1694
Pen y Fan <{ID}+{username}@users.noreply.github.com>
2019-02-08 18:57:59 +0000

    Product create

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Resources\Product\ProductResource;
+use App\Exceptions\ProductNotBelongsToUser;
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
+use App\Http\Resources\Product\ProductResource;
 use App\Model\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class ProductController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index', 'show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -35,9 +43,21 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $product = new Product();
+        $product->name = $request->name;
+        $product->detail = $request->description;
+        $product->stock = $request->stock;
+        $product->price = $request->price;
+        $product->discount = $request->discount;
+        $product->save();
+        return response(
+            [
+                'data' => new ProductResource($product)
+            ],
+            Response::HTTP_CREATED
+        );
     }
 
     /**
@@ -52,6 +72,17 @@ class ProductController extends Controller
     }
 
     /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Product $product)
+    {
+        //
+    }
+
+    /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -60,7 +91,16 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        $this->ProductUserCheck($product);
+        $request['detail'] = $request->description;
+        unset($request['description']);
+        $product->update($request->all());
+        return response(
+            [
+                'data' => new ProductResource($product)
+            ],
+            Response::HTTP_CREATED
+        );
     }
 
     /**
@@ -71,6 +111,15 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $this->ProductUserCheck($product);
+        $product->delete();
+        return response(null, Response::HTTP_NO_CONTENT);
+    }
+
+    public function ProductUserCheck($product)
+    {
+        if (Auth::id() !== $product->user_id) {
+            throw new ProductNotBelongsToUser();
+        }
     }
 }
